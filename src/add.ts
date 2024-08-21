7343a40088c9de1739b56b4321ba24f1c33218a2
mehraDev <126305156+mehraDev@users.noreply.github.com>
2024-08-21 20:30:38 +0530

    Implement add function to handle custom delimiter

--- src/add.ts
+++ src/add.ts
@@ -2,5 +2,14 @@ export function add(numbers: string): number {
     if(numbers === ''){
         return 0;
     }
+
+    if(numbers.startsWith('//')){
+        const delimiterEndIndex = numbers.indexOf('\n');
+        const delimiter = numbers.substring( 2, delimiterEndIndex);
+        const numbersString = numbers.substring(delimiterEndIndex + 1);
+
+        return numbersString.split(delimiter).map(num => parseInt(num, 10)).reduce((a,b) => a + b , 0);
+    }
+
     return numbers.split(/[\n,]/).map(num => parseInt(num, 10)).reduce((a,b) => a + b , 0);
 }
