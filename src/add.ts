0a6da7f6bb8e37ebaea7eb4c3205cdbdccc08abb
mehraDev <126305156+mehraDev@users.noreply.github.com>
2024-08-21 20:19:45 +0530

    Implement add function to handle new line between numbers

--- src/add.ts
+++ src/add.ts
@@ -2,5 +2,5 @@ export function add(numbers: string): number {
     if(numbers === ''){
         return 0;
     }
-    return numbers.split(',').map(num => parseInt(num, 10)).reduce((a,b) => a + b , 0);
+    return numbers.split(/[\n,]/).map(num => parseInt(num, 10)).reduce((a,b) => a + b , 0);
 }
